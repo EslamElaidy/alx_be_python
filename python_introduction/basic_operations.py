d9fbeb9ff4ae42644c3496fd98090d27f5151a88
EslamElaidy <eslam.ayde@gmail.com>
2024-09-14 01:02:14 +0300

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,8 @@
-
+number1 = int(10)
+number2 = int(5)
+result1 = number1 + number2
+result2= number1 - number2
+result3 = number1 * number2
+print ("Addition of 10 and 5 is", result1)
+print ("Subtraction of 10 and 5 is", result2)
+print ("Multiplication of 10 and 5 is", result3)
