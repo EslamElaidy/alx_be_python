aa072f2531283fc31b6e5d4aa9e445424795a3ff
EslamElaidy <eslam.ayde@gmail.com>
2024-09-23 02:02:00 +0300

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,9 +1,5 @@
-# Prompt User for a Number
 x = int(input("Enter a number to see its multiplication table: "))
-
-# Generate and Print the Multiplication Table
 Y_list = range(1, 11)
-
 for y in Y_list:  # Added the colon here
     product = x * y
-    print(f"{number} * {y} = {product}")
+    print(product)
