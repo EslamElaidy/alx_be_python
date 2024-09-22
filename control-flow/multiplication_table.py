44c1fc3966d0e800a85d3c558791cd406a38bb48
EslamElaidy <eslam.ayde@gmail.com>
2024-09-23 02:10:15 +0300

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 X = int(input("Enter a number to see its multiplication table: "))
-Y_list = range(1, 11)
+Y_list = range(1, 10)
 for Y in Y_list:  # Added the colon here
     Z = X * Y
     print(Z)
