cf3e37ef1051aabf2143542c657def290737993d
EslamElaidy <eslam.ayde@gmail.com>
2024-09-23 02:03:41 +0300

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,6 @@
 x = int(input("Enter a number to see its multiplication table: "))
 Y_list = range(1, 11)
 for y in Y_list:  # Added the colon here
-    product = x * y
-    print(product)
+    if y < 11
+    print(x*y)
+    print()
