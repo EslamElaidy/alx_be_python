0120beaca8b58c5428ef25a60e0d804d554fdab4
EslamElaidy <eslam.ayde@gmail.com>
2024-09-30 00:57:48 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,14 @@
 def perform_operation(num1, num2, operation):
-  match operation:
-    case "add":
-      return num1 + num2
-    case "subtract":
+  if operation == "add":
+    return num1 + num2
+  elif operation == "subtract":
       return num1 - num2
-    case "multiply":
+  elif operation == "multiply":
       return num1 * num2
-    case "divide":
+  elif opertaion == "divide":
       if num2 != 0:
         return num1 / num2
       else:
         print("this is not valid")
+  else:
+    return "invalid operation"
