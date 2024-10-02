b602379530de7f1886ab4cc7ecf981ab5d601830
EslamElaidy <eslam.ayde@gmail.com>
2024-10-03 01:06:25 +0300

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,1 +1,8 @@
-
+:FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+def convert_to_celsius(fahrenheit):
+  celsius = fahrenheit * 5/9
+def convert_to_fahrenheit(celsius):
+  fahrenheit = celsius * 9/5
+temp_unit = int(input("press 1 for c and 2 for f"))
+temp = int(input("Enter your degree"))
