ce95e93dd3c7244f25d7d2eee64950498906d85e
EslamElaidy <eslam.ayde@gmail.com>
2024-10-09 01:50:42 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,4 +1,14 @@
 class BankAccount:
-  def __init__(self, initial_balance)
-  initial_balance = 0
-  self.account_balance = amount
+  def __init__(self, initial_balance):
+    initial_balance = 0
+    self.account_balance = initial_balance
+  def deposit(self,amount):
+    if amount > 0:
+      self.account_balance += amount
+      print (f"your balance is {amount}")
+    else:
+      print("invalid value")
+  def withdraw(self,amount):
+    if amount > 0 and amount <= account_balance
+    self.account_balance -= amount
+    print (f"your balance is {amount}")
