3ea4f92b9e032f93452f0aea6d671d8a4b31afff
EslamElaidy <eslam.ayde@gmail.com>
2024-10-11 01:55:15 +0300

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,8 @@
 def safe_divide(numerator, denominator):
   try:
-    result = numerator / denominator
+  numerator = float(numerator)
+  denominator = float(denominator)
+  result = numerator / denominator
 except ZeroDivisionError:
   return "Invalid number"
 else:
